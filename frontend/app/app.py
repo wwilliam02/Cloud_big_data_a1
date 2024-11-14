d0b23e209d0a23c9b4badde12450555bf5ad7d80
wwilliam02 <wg02@live.se>
2024-11-14 14:39:03 +0100

    frontend test working

--- frontend/app/app.py
+++ frontend/app/app.py
@@ -7,9 +7,36 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-def greet():
+@app.route("/addConsert", methods=["POST"])
+def addConsert():
     data = request.get_json()
-    name = data.get("name", "World")
-    return jsonify({"message":f"Hello, {name}!"})
+    #format and send to backend
 
-if
+    artist = data.get("artist")
+    venue = data.get("venue")
+    date = data.get("date")
+    tour = data.get("tour")
+
+
+    event_data = {
+        "artist": artist,
+        "venue": venue,
+        "date": date,
+        "tour": tour
+    }
+
+    #send event_data to backend...
+    print(event_data)
+
+     # Process the data (you could store it in a database or just print it for now)
+    print(f"Artist: {artist}, Venue: {venue}, Date: {date}, Tour: {tour}")
+
+    # Respond back to the client
+    return jsonify({"message": "Event data received successfully", "data": event_data})
+    #return jsonify({"message": "Message sent", "data":event_data})
+    
+
+
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port=5000)
